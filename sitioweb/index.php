e4ae04cc18ca3cbf8e0f505934cb773d5e1663fa
Mateovcs <130392952+Mateovcs@users.noreply.github.com>
2024-12-06 17:47:52 -0300

    Add files via upload

---
+++ sitioweb/index.php
@@ -0,0 +1,18 @@
+<?php include("template/cabecera.php"); ?>
+
+        <div class="jumbotron text-center">
+            <h1 class="display-3">Bienvenido a esta Biblioteca Virtual</h1> <!-- INICIO DE LA PAGINA, OSEA LA BIENVENIDA PARA TODO PUBLICO, NO ADMINISTRADOR !-->
+            <p class="lead">Consulta los libros que dispone esta Biblioteca</p>
+            <hr class="my-2">
+        <img width="400" src="img/vieja.jpg" class="img-thumbnail rounded mx-auto d-block" />
+
+            <p>Más Información debajo de esto</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="productos.php" role="button">Ver estanteria de libros</a>
+            </p>
+        </div>
+
+<?php include("template/pie.php"); ?>
+
+
+      
---
+++ sitioweb/nosotros.php
@@ -0,0 +1,10 @@
+<?php include("template/cabecera.php"); ?>
+
+<div class="jumbotron">
+    <h1 class="display-3">Nosotros</h1> 
+    <p class="lead">Hola, somos una biblioteca en la que no hay presupuesto para tener personal, asi que lo administra una sola persona </p>
+    <img src="" alt="">
+    <hr class="my-2">
+</div>
+
+<?php include("template/pie.php"); ?>
